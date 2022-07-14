61e8a704318ae1380eb3e10e4e6ff8741406ae1a
Tanmay Rauth <tanmayrauth@gmail.com>
2022-07-14 03:43:21 +0000

    added api

--- model.py
+++ model.py
@@ -103,15 +103,15 @@ def decode_sequence(input_seq):
     #return the deocded sentence
     return decoded_sentence
 
-inp_review = input("Enter : ")
-print("Review :",inp_review)
+# inp_review = input("Enter : ")
+# print("Review :",inp_review)
 
-inp_review = clean(inp_review,"inputs")
-inp_review = ' '.join(inp_review)
-inp_x= in_tokenizer.texts_to_sequences([inp_review]) 
-inp_x= pad_sequences(inp_x,  maxlen=max_in_len, padding='post')
+# inp_review = clean(inp_review,"inputs")
+# inp_review = ' '.join(inp_review)
+# inp_x= in_tokenizer.texts_to_sequences([inp_review]) 
+# inp_x= pad_sequences(inp_x,  maxlen=max_in_len, padding='post')
 
-summary=decode_sequence(inp_x.reshape(1,max_in_len))
-if 'eos' in summary :
-  summary=summary.replace('eos','')
-print("\nPredicted summary:",summary);print("\n")
+# summary=decode_sequence(inp_x.reshape(1,max_in_len))
+# if 'eos' in summary :
+#   summary=summary.replace('eos','')
+# print("\nPredicted summary:",summary);print("\n")
--- server.py
+++ server.py
@@ -9,14 +9,24 @@ app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
 
 
-@app.route('/api',methods=['POST'])
+@app.route('/api/summarise', methods=['POST'])
 def summarize():
     # Get the data from the POST request.
     data = request.get_json(force=True)
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['exp'])]])
-    # Take the first value of prediction
-    output = prediction[0]
+    # Make prediction using model loaded from disk as per the data. 
+    print("Review :",inp_review)
+
+    inp_review = clean(inp_review,"inputs")
+    inp_review = ' '.join(inp_review)
+    inp_x      = in_tokenizer.texts_to_sequences([inp_review]) 
+    inp_x      = pad_sequences(inp_x,  maxlen=max_in_len, padding='post')
+
+    summary    = decode_sequence(inp_x.reshape(1,max_in_len))
+
+    if 'eos' in summary :
+    summary=summary.replace('eos','')
+    print("\nPredicted summary:",summary);print("\n")
+
     return jsonify(output)
 
 
