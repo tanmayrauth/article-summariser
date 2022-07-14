b93f94e1b9ebd40e3af3767547bb46afca064ae0
Tanmay Rauth <tanmayrauth@gmail.com>
2022-07-14 03:51:08 +0000

    server setup

--- server.py
+++ server.py
@@ -1,14 +1,13 @@
 # Import libraries
 import numpy as np
-from flask import Flask, request, jsonify
 import pickle
-
+from flask import Flask, request, jsonify
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from utils import clean
+from model import decode_sequence
 
 app = Flask(__name__)
-# Load the model
-model = pickle.load(open('model.pkl','rb'))
-
-
+  
 @app.route('/api/summarise', methods=['POST'])
 def summarize():
     # Get the data from the POST request.
@@ -24,8 +23,8 @@ def summarize():
     summary    = decode_sequence(inp_x.reshape(1,max_in_len))
 
     if 'eos' in summary :
-    summary=summary.replace('eos','')
-    print("\nPredicted summary:",summary);print("\n")
+        summary=summary.replace('eos','')
+        print("\nPredicted summary:",summary);print("\n")
 
     return jsonify(output)
 
